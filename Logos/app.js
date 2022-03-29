364469943f46f844c721be103140dfc4000168a9
JimmiJan <96634490+JimmiJan@users.noreply.github.com>
2022-03-29 23:42:23 +0500

    Add files via upload

---
+++ Logos/app.js
@@ -0,0 +1,15 @@
+// let linkss = document.getElementById("links")
+//  document.getElementById("manu").onclick=()=>{
+//     linkss.classList.toggle("active")
+//     console.log(linkss)
+//  }
+ 
+function myFunction(){
+    let x = document.getElementById("Timmers") 
+    if(x.innerHTML === "Jamshed"){
+        x.innerHTML = "Muhammad Jamshed"
+    }else{
+        x.innerHTML = "Jamshed"
+    }
+}
+
